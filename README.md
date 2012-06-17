8f5066cc6c06b623197ba73bd313b4c64e8509fa
Tarabukka <ds@blockcommunity.net>
2012-06-17 16:19:09 +0100

    Updated README. Removed moved files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 p1nk
 ====
 
-P1NK PHP framework
+P1NK PHP framework. Requires PHP 5.3 and above and PDO with MySQL support.
--- application/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	class Config {
-		const QUERY_PARAM = 'q';
-		const APP_BASE = 'http://localhost/pink/';
-		const TIMEZONE = 'Europe/London';
-	}
-?>
